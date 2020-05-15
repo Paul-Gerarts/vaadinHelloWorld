d44a4665581e3ef6757ba5800d283e8d9753cdb0
Paul Gerarts <paul.gerarts@dexis.be>
2020-05-15 14:09:09 +0200

    Added toDo form Extracted GreeterForm

---
+++ src/main/java/be/juvo/paul/ToDoForm.java
@@ -0,0 +1,31 @@
+package be.juvo.paul;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+
+public class ToDoForm extends VerticalLayout {
+
+    public ToDoForm() {
+        VerticalLayout todoList = new VerticalLayout();
+        TextField taskField = new TextField();
+        Button addButton = new Button("Add");
+        addButton.addClickShortcut(Key.ENTER);
+        addButton.addClickListener(click -> {
+            Checkbox checkbox = new Checkbox(taskField.getValue());
+            todoList.add(checkbox);
+        });
+        add(
+                new H1("Vaadin Todo"),
+                todoList,
+                new HorizontalLayout(
+                        taskField,
+                        addButton
+                )
+        );
+    }
+}
